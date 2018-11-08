51e05a14aab0e4cff5f5ed704233b86f1d4df9c5
Benjamin A. Petersen <ben@benjaminapetersen.me>
2018-11-08 16:07:26 -0500

    Create ClusterOperatorStatus in starter.go

--- pkg/console/starter/starter.go
+++ pkg/console/starter/starter.go
@@ -2,28 +2,50 @@ package starter
 
 import (
 	"fmt"
-	"github.com/openshift/library-go/pkg/operator/status"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/tools/cache"
 	"time"
 
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
+	"github.com/openshift/library-go/pkg/operator/status"
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
 	"github.com/openshift/console-operator/pkg/console/operator"
 	"github.com/openshift/console-operator/pkg/generated/clientset/versioned"
 	"github.com/openshift/console-operator/pkg/generated/informers/externalversions"
 )
 
+
+// Time to wire up our informers/clients/etc
+//
+// informers
+// - listen for changes
+//
+// clients
+// - used by informers to get,list,put resources
+//
+// informers
+// - instantiated in starter.go (here)
+// - consumed in... places.
 func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
+
+	fmt.Printf("starter.go -> RunOperator() func........\n")
+
+	// creates a new kube clientset
+	// clientConfig is a REST config
+	// a clientSet contains clients for groups.
+	// each group has one version included in the set.
 	kubeClient, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
 		return err
 	}
 
+	// pkg/apis/console/v1alpha1/types.go has a `genclient` annotation,
+	// that creates the expected functions for the type.
 	consoleOperatorClient, err := versioned.NewForConfig(clientConfig)
 	if err != nil {
 		return err
@@ -32,24 +54,39 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 	const resync = 10 * time.Minute
 
 	// only watch a specific resource name
+	// this is an optimization step that is not initially needed
+	// TODO: eliminate for now?
 	tweakListOptions := func(options *v1.ListOptions) {
 		options.FieldSelector = fields.OneTermEqualSelector("metadata.name", operator.ResourceName).String()
 	}
 
-	kubeInformersNamespaced := informers.NewSharedInformerFactoryWithOptions(kubeClient, resync,
+	kubeInformersNamespaced := informers.NewSharedInformerFactoryWithOptions(
+		// takes a client
+		kubeClient,
+		resync,
+		// takes an unlimited number of additional "options" arguments, which are functions,
+		// that take a sharedInformerFactory and return a sharedInformerFactory
 		informers.WithNamespace(operator.TargetNamespace),
 		informers.WithTweakListOptions(tweakListOptions),
 	)
 
-	consoleOperatorInformers := externalversions.NewSharedInformerFactoryWithOptions(consoleOperatorClient, resync,
+	consoleOperatorInformers := externalversions.NewSharedInformerFactoryWithOptions(
+		// this is our generated client
+		consoleOperatorClient,
+		resync,
+		// and the same set of optional transform functions
 		externalversions.WithNamespace(operator.TargetNamespace),
 		externalversions.WithTweakListOptions(tweakListOptions),
 	)
 
 	consoleOperator := operator.NewConsoleOperator(
+		// informer factory, drilling down to the v1alpha1 console informer
 		consoleOperatorInformers.Console().V1alpha1().Consoles(),
+		// specifically secrets, we can add more here.
 		kubeInformersNamespaced.Core().V1().Secrets(),
+		// client and informer are NOT the same
 		consoleOperatorClient.ConsoleV1alpha1().Consoles(operator.TargetNamespace),
+		// client and informer are NOT the same
 		kubeClient.CoreV1(),
 	)
 
@@ -68,17 +105,18 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 	// to test this, will need to update STATUS on my CR
 	// and then check and see if it is updated on the other deal.
 	// TODO: install this, from the old console-operator install  operatorstatus.openshift.io
-	//clusterOperatorStatus := status.NewClusterOperatorStatusController(
-	//	// what are these? name of controller(operator)? wire this up.
-	//	"openshift-apiserver",
-	//	"openshift-apiserver",
-	//	// what is this
-	//	dynamicClient,
-	//	&operatorStatusProvider{informers: operatorConfigInformers},
-	//)
-	//
-	//// TODO: will have a series of runs here
-	//go clusterOperatorStatus.Run(1, stopCh)
+	clusterOperatorStatus := status.NewClusterOperatorStatusController(
+		// TODO: these may not be exactly right...
+		// make sure they are what we actually need.
+		operator.TargetNamespace,
+		operator.ResourceName,
+		// for some reason we need a dynamic client.  this is weird, and i dont know why
+		// given that we rant about "real" strongly typed clients constantly.
+		dynamicClient,
+		&operatorStatusProvider{informers: consoleOperatorInformers},
+	)
+	// TODO: will have a series of Run() funcs here
+	go clusterOperatorStatus.Run(1, stopCh)
 
 	<-stopCh
 
@@ -86,23 +124,22 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 }
 
 
+// NOTE: i want this in a separate package,
+// but most other operators seem to keep it here :/
+type operatorStatusProvider struct {
+	informers externalversions.SharedInformerFactory
+}
 
-// Interface
-//type operatorStatusProvider struct {
-//	informers operatorclientinformers.SharedInformerFactory
-//}
-//
-//func (p *operatorStatusProvider) Informer() cache.SharedIndexInformer {
-//	// TODO: return my informaer....
-//	// return p.informers.Openshiftapiserver().V1alpha1().OpenShiftAPIServerOperatorConfigs().Informer()
-//}
-//
-//func (p *operatorStatusProvider) CurrentStatus() (operatorv1alpha1.OperatorStatus, error) {
-//	// TODO: use my informer to get my status
-//	instance, err := p.informers.Openshiftapiserver().V1alpha1().OpenShiftAPIServerOperatorConfigs().Lister().Get("instance")
-//	if err != nil {
-//		return operatorv1alpha1.OperatorStatus{}, err
-//	}
-//
-//	return instance.Status.OperatorStatus, nil
-//}
+func (p *operatorStatusProvider) Informer() cache.SharedIndexInformer {
+	return p.informers.Console().V1alpha1().Consoles().Informer()
+}
+
+func (p *operatorStatusProvider) CurrentStatus() (operatorv1alpha1.OperatorStatus, error) {
+	instance, err := p.informers.Console().V1alpha1().Consoles().Lister().Consoles(operator.TargetNamespace).Get(operator.ResourceName)
+	if err != nil {
+		return operatorv1alpha1.OperatorStatus{}, err
+	}
+
+	return instance.Status.OperatorStatus, nil
+}
+
